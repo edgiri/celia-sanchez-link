330422188839b4e49419009801d48a0c45dd423a
edgiri <auraxmgmt@gmail.com>
2026-07-28 21:20:34 +0400

    fix a tag

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ const IconFB = () => (
 
 function SocialButton({ href, children }: { href: string; children: React.ReactNode }) {
   return (
-    
+    <a
       href={href}
       target={"_blank"}
       rel={"noreferrer"}
